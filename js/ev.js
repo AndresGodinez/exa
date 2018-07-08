1e814337a47fca0c8fe64c35bb768660041156c5
AGodinez <ing.a.godinez@gmail.com>
2018-07-08 11:52:56 -0500

    first commit

---
+++ js/ev.js
@@ -0,0 +1,44 @@
+//declaración de variables
+var title = "Gastos de casa";
+var renta = 3000;
+var agua = 200;
+var luz = 500;
+var colegiatura = 2000;
+var inscripcion = 1500; 
+var comida = 2000;
+var transporte = 400;
+
+// console.log(gastos[position]);
+
+var gastos = [renta, agua, luz, colegiatura, inscripcion, comida, transporte];
+
+var suma = 0;
+for(var i = 0; i<6; i++){
+	console.log(gastos[i]);
+	suma = suma + gastos[i];
+
+}
+console.log(suma);
+
+
+
+
+
+//propiedades de arrays
+
+
+//incremetar elementos
+
+
+//ordenamiento
+
+
+
+
+
+
+
+
+
+
+//eliminación
