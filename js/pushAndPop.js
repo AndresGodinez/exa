dd4380c5fca87f0b7048bd34fcd05206787916a0
AGodinez <ing.a.godinez@gmail.com>
2018-07-15 11:24:30 -0500

    array push and pop

---
+++ js/pushAndPop.js
@@ -0,0 +1,21 @@
+// Push and Pop en arreglos
+
+// var arreglo = [1, 2, "antonio", 3, true];
+
+// console.log(arreglo);
+//length tamaño
+// console.log("el tamaño del arreglo es " + arreglo.length);
+
+// hacer un arreglo de calificaciones 1,2,3 
+//imprimir el arreglo y imprimir el tamaño del arreglo
+
+var calificaciones = [8, 9, 10];
+
+console.log(calificaciones);
+console.log(calificaciones.length);
+
+calificaciones.push(10);
+calificaciones.push(5);
+calificaciones.push(2);
+console.log(calificaciones);
+console.log(calificaciones.length);
